4e5c59a7389c5473a5dcbdf3f01cb8c36f548d3f
VIVEK SINGH <77235217+viveksingh31722@users.noreply.github.com>
2025-07-22 15:21:14 +0530

    Project setup and Added Shadcn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,19 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "prettier",
+      "plugin:tailwindcss/recommended",
+    ],
+    plugins: ["prettier"],
+    rules: {
+      "prettier/prettier": "error",
+      "react/no-escape-entities": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
